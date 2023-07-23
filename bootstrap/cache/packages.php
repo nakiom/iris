0e7a5c3e0da3c61a61d64523bf7f4c2b2172f110
nakiom <nachio@gmail.com>
2023-07-23 09:49:37 -0300

    upgrade laravel 10

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -66,6 +66,13 @@
       0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
     ),
   ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
   'spatie/laravel-ignition' => 
   array (
     'providers' => 
